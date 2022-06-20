296c795733fe8cd521231f62396f3d33809821bd
ANDYWANGTIANTIAN <53755609+ANDYWANGTIANTIAN@users.noreply.github.com>
2022-06-20 19:26:39 +0800

    地图添加背景，但运行中不能生效

---
+++ Resources/pink.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="pink" tilewidth="16" tileheight="16" tilecount="52547" columns="187">
+ <image source="../../../大作业/pink.jpg" width="3000" height="4500"/>
+</tileset>
